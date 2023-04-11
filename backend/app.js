8b6081077dd557ba31eafff2e434d9d7c9f0ef85
Ahmed Mrissa <amrissa1@gmail.com>
2023-04-11 18:02:49 -0400

    Implement Firebase for Uploading files alongside with multer

--- backend/app.js
+++ backend/app.js
@@ -22,7 +22,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(morgan('tiny'))
 app.use(morgan('tiny', { stream: stream }))
 app.use(function(req, res, next) {
-        res.header("Access-Control-Allow-Origin", "https://mrissabackend.vercel.app"); // update to match the domain you will make the request from
+        res.header("Access-Control-Allow-Origin", "https://ahmedmrissa.vercel.app"); // update to match the domain you will make the request from
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
         next();
       });
